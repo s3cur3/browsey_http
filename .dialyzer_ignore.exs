eefce9cb7a4e90c34530c962864a63b85395d624
Tyler Young <tyler@tylerayoung.com>
2024-06-19 12:34:46 -0500

    Fix dialyzer errors

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -5,4 +5,10 @@
 #
 # More info in the Dialyxir README:
 # https://github.com/jeremyjh/dialyxir#elixir-term-format
-[]
+[
+  # :dockerexec.run/3 *can* return other success results, but based on the arguments
+  # we've hard-coded, we know the result will always be this structure.
+  {"lib/browsey_http/util/exec.ex", :missing_range},
+  # Dialyzer thinks dockerexec will always be there
+  {"lib/browsey_http/util/exec.ex", :pattern_match}
+]
